381e523be6e3beb95840f77353d871ea7e15db18
ITStart <ITStart@223-4>
2023-01-17 21:35:01 +0600

    Добавьте файлы проекта.

---
+++ ExampleDemoWpf/CountriesCards.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ExampleDemoWpf
+{
+    /// <summary>
+    /// Логика взаимодействия для CountriesCards.xaml
+    /// </summary>
+    public partial class CountriesCards : Window
+    {
+        Country country = new Country();
+        public string ContinentsChange { get; set; }
+
+        private ObservableCollection<ProbaList> _objectList;
+        public ObservableCollection<ProbaList> ListBoxDataSource { get; set; }
+
+
+        public CountriesCards()
+        {
+            InitializeComponent();
+
+            _objectList = new ObservableCollection<ProbaList>()
+            {
+                 new ProbaList
+                 {
+                     UrlPhoto = "\\Photo\\Kazakhstan.jpg",
+                     CountryName = "Kazakhstan"
+                 },
+                 new ProbaList
+                 {
+                     UrlPhoto = "\\Photo\\Kazakhstan.jpg",
+                     CountryName = "Uzbekistan"
+                 }
+
+            };
+
+            phonesList.ItemsSource = _objectList;
+
+        }
+
+        private void phonesList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            OpenNewWindow("Kazakhstan");
+        }
+
+        private void OpenNewWindow(String countryChange)
+        {
+            country.CountryChange = countryChange;
+            this.Close();
+            country.Show();
+        }
+    }
+}
